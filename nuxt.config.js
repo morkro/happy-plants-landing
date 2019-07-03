13121dc294745c07742d1245d55117954d86398d
Moritz Kröger <write@morkro.de>
2019-07-03 22:14:00 +0100

    :tada: Adds Nuxt.js setup

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+import StyleLintPlugin from 'stylelint-webpack-plugin'
+
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', '@nuxtjs/eslint-module'],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      config.plugins.push(
+        StyleLintPlugin({
+          files: '**/*.vue'
+        })
+      )
+    },
+
+    postcss: {
+      plugins: {
+        'postcss-import': {},
+        'postcss-url': {},
+        'postcss-preset-env': {
+          stage: 0,
+            features: {
+            calc: { preserve: true },
+            'nesting-rules': true,
+            'color-mod': true,
+            'custom-media': true
+          }
+        },
+        'postcss-reporter': {},
+        'postcss-color-mod-function': {},
+      }
+    }
+  }
+}
